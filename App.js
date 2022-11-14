9b1874d6850abd77788bff7062d5a0aa908425ac
Rocinate <rocinate1999@gmail.com>
2022-11-14 22:54:34 +0800

    add launch screen

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import PermissionRequire from './src/PermissionRequire';
 import Layout from './src/page/Layout';
 import {StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
   const [ready, setReady] = useState(false);
+  // let timerId = null
+  // const [done, setDone] = useState(false);
+
+  useEffect(() => {
+    SplashScreen.show()
+    setTimeout(() => {
+      SplashScreen.hide()
+    }, 3000)
+  }, [])
+
   return (
     <SafeAreaProvider>
       <StatusBar hidden={true} />
